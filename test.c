d6c341ce9d4457d5bbd1f2710f646739f1d9f163
wwickstrum <wwickstrum@gmail.com>
2026-01-28 14:33:45 -0500

    finshed 0-10, 2-20, half way through even numbers 2-40

--- test.c
+++ test.c
@@ -1,2 +1,48 @@
 #include <stdio.h>
 
+void print10();
+void print20();
+void print440();
+
+int main()
+{
+    print10();
+    print20();
+    print440();
+}
+
+void print10()
+{
+    for (int i = 0; i <= 10; i++)
+    {
+        printf("%d\n", i);
+    }
+}
+
+void print20()
+{
+    for(int i = 2; i <= 20; i++)
+    {
+        printf("%d\n", i);
+
+    }
+
+}
+
+void print440()
+{
+    for(int i=4; i<= 40; i++)
+    {
+        printf("%d")
+    }
+
+
+}
+
+
+
+
+
+
+
+
