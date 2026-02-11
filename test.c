9d48f68f58fd1bddf443c296dfa3470c62346673
wwickstrum <wwickstrum@gmail.com>
2026-02-11 14:59:06 -0500

    new python file

--- test.c
+++ test.c
@@ -1,46 +1,15 @@
 #include <stdio.h>
 
-void print10();
-void print20();
-void print440();
+int array85[85];
+int matrix20[20][20];
 
 int main()
 {
-    print10();
-    print20();
-    print440();
-}
-
-void print10()
+for (int i=0; i<21; i++);
 {
-    for (int i = 0; i <= 10; i++)
-    {
-        printf("%d\n", i);
-    }
+    for (int j=0; j<21; j++);
 }
-
-void print20()
-{
-    for(int i = 2; i <= 20; i++)
-    {
-        printf("%d\n", i);
-
-    }
-
 }
-
-void print440()
-{
-    for(int i=4; i<= 40; i++)
-    {
-        printf("%d")
-    }
-
-
-}
-
-
-
 
 
 
