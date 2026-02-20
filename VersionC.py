9cbfe7bd207adc3828217d807de515a77ad93986
wwickstrum <wwickstrum@gmail.com>
2026-02-20 14:28:29 -0500

    VersionCfinish

--- VersionC.py
+++ VersionC.py
@@ -1,2 +1,35 @@
 greeting="Welcome"
 print(greeting)
+
+Width=7
+length=3
+print(Width*length)
+print(Width+length)
+print(Width/length)
+print(Width/length)
+
+radius=10
+print(2*3.14159*radius)
+
+print(2/(5+5))
+
+is_valid=True
+
+i=100
+j=100
+print(i>=j)
+print(i<j)
+
+age=16
+has_permit=True
+if age>=16 and has_permit:
+    print("you can drive")
+else:
+    print("you cant drive")
+
+user= "linus"
+task= "kernel"
+print(f"{user} created the {task}")
+
+balance=1234.5678
+print(f"current balance: ${balance}")
